7f517efbf69537d87c5bbef6f54b298ce1388901
seondal <sseondal@gmail.com>
2025-01-23 17:26:33 +0900

    Query Type

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({});
+// Schema Dafinition Language
+const typeDefs = gql`
+  # 사용자의 모든 요청
+  type Query {
+    # GET /text
+    text: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
