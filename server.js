4f3f9e46541c10d86d06e1cf56da56265e357cc2
seondal <sseondal@gmail.com>
2025-01-23 17:55:59 +0900

    Scalar, Root Types

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ import { ApolloServer, gql } from "apollo-server";
 
 // Schema Dafinition Language
 const typeDefs = gql`
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
+    text: String
+    author: User
+  }
+
   # 사용자의 모든 요청
   type Query {
-    # GET /text
-    text: String
+    # GET /allTweets
+    allTweets: [Tweet]
+
+    # GET /tweet/:id
+    tweet(id: ID): Tweet
   }
 `;
 
